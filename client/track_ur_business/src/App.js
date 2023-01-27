8a7d1237aed45aee3d454ba6c3a0e6a7153fbbb6
prajpurohit680 <prajpurohit680@gmail.com>
2023-01-27 22:03:28 +0530

    google button added

--- client/track_ur_business/src/App.js
+++ client/track_ur_business/src/App.js
@@ -1,9 +1,18 @@
-
+import GoogleButton from 'react-google-button';
+import { useNavigation } from '@react-navigation/native';
 
 function App() {
+  const navigator= useNavigation();
+
+  
+
   return (
     <div className="App">
       <h1> Commit</h1>
+      <GoogleButton
+        type="light" // can be light or dark
+        onClick={() => { console.log('Google button clicked') }}
+      />
     </div>
   );
 }
